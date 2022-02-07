21e3afa401d79747a8af36c5b169562dae6cf4dd
Iury Ferreira <iuryferreiralol@gmail.com>
2022-02-06 21:40:18 -0300

    middleware fetch

--- pages/app/_middleware.ts
+++ pages/app/_middleware.ts
@@ -3,14 +3,9 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 const hostUrl = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
 
 export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
-  // const refreshCookie = req.cookies['refreshToken'];
+  const refreshCookie = req.cookies['refreshToken'];
 
-  const res = await fetch(`${hostUrl}/api/users/me`, {
-    // credentials: 'include',
-    headers: req.headers,
-  });
-
-  if (res.status !== 200) {
+  if (!refreshCookie) {
     return NextResponse.redirect(`${hostUrl}/auth/login`);
   }
 
--- pages/auth/_middleware.ts
+++ pages/auth/_middleware.ts
@@ -3,14 +3,11 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 const hostUrl = process.env.NEXT_PUBLIC_SERVER_ENDPOINT;
 
 export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
-  // const res = await fetch(`${hostUrl}/api/users/me`, {
-  //   credentials: 'include',
-  //   headers: req.headers,
-  // });
+  const refreshCookie = req.cookies['refreshToken'];
 
-  // if (res.status === 200) {
-  //   return NextResponse.redirect(`${hostUrl}/app`);
-  // }
+  if (refreshCookie) {
+    return NextResponse.redirect(`${hostUrl}/app`);
+  }
 
   return NextResponse.next();
 };
